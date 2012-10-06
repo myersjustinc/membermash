4d732e5b85e55264226ba7fee2377634adf4c617
Justin Myers <justin@justinmyers.net>
2012-10-06 16:43:00 -0400

    Added CRP contributor methods

---
+++ membermash/legislators/details.py
@@ -0,0 +1,85 @@
+import logging
+from operator import itemgetter
+import os
+import requests
+import sunlight
+import sys
+
+try:
+    import json
+except ImportError:
+    import simplejson as json
+
+try:
+    sunlight.config.API_KEY = os.environ['SUNLIGHT_API_KEY']
+    CRP_API_KEY = os.environ['CRP_API_KEY']
+except KeyError:
+    sys.stderr.write("Had trouble with your data API keys.\n")
+    sys.exit(1)
+
+logging.getLogger(__name__)
+
+def get_industry_contributions(crp_id, late_cycle=2012, early_cycle=2000):
+    API_ENDPOINT = 'http://www.opensecrets.org/api/'
+    cycle = late_cycle
+    while cycle and cycle > early_cycle:
+        crp_params = {
+            "method": "candIndustry",
+            "output": "json",
+            "apikey": CRP_API_KEY,
+            "cycle": str(cycle),
+            "cid": crp_id,
+        }
+        r = requests.get(API_ENDPOINT, params=crp_params)
+        
+        try:
+            raw_results = json.loads(r.text)
+            industries = [item["@attributes"] for item in 
+                raw_results["response"]["industries"]["industry"]]
+            for item in industries:
+                item["indivs"] = int(item["indivs"])
+                item["pacs"] = int(item["pacs"])
+                item["total"] = int(item["total"])
+            industries.sort(key=itemgetter("total"), reverse=True)
+        except (KeyError, ValueError), e:
+            logger.error("Industries error for CRP ID %s" % crp_id)
+            return []  # Not sure what happened here.
+        
+        if False:  # FIXME: This should be the no-results-found check.
+            cycle -= 2  # Go to the previous election cycle
+        else:
+            return industries  # Also breaks us out of the loop
+    
+    logger.info("No industries results for CRP ID %s" % crp_id)
+    return []  # If we're here, we've definitely got nothing.
+
+def get_contributors(crp_id, late_cycle=2012, early_cycle=2000):
+    API_ENDPOINT = 'http://www.opensecrets.org/api/'
+    cycle = late_cycle
+    while cycle and cycle > early_cycle:
+        crp_params = {
+            "method": "candContrib",
+            "output": "json",
+            "apikey": CRP_API_KEY,
+            "cycle": str(cycle),
+            "cid": crp_id,
+        }
+        r = requests.get(API_ENDPOINT, params=crp_params)
+        
+        try:
+            raw_results = json.loads(r.text)
+            industries = [item["@attributes"] for item in 
+                raw_results["response"]["contributors"]["contributor"]]
+            for item in industries:
+                item["total"] = int(item["total"])
+        except (KeyError, ValueError), e:
+            logger.error("Contributors error for CRP ID %s" % crp_id)
+            return []  # Not sure what happened here.
+        
+        if False:  # FIXME: This should be the no-results-found check.
+            cycle -= 2  # Go to the previous election cycle
+        else:
+            return industries  # Also breaks us out of the loop
+    
+    logger.info("No contributors results for CRP ID %s" % crp_id)
+    return []  # If we're here, we've definitely got nothing.
