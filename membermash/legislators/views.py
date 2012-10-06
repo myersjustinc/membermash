4f606a69fd2252c03411a78b1a1053d7db747b92
Justin Myers <justin@justinmyers.net>
2012-10-06 17:55:25 -0400

    Added random mash support

--- membermash/legislators/views.py
+++ membermash/legislators/views.py
@@ -1,5 +1,5 @@
 from django.http import Http404
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 
 from .models import Legislator
 
@@ -10,4 +10,10 @@ def mash(request, bioguide_1=None, bioguide_2=None):
     raise Http404("mash")
 
 def random(request):
-    raise Http404("random")
+    active_random = Legislator.objects.filter(is_active=True).order_by('?')
+    legislator_1 = active_random[0]
+    legislator_2 = active_random.filter(chamber=legislator_1.chamber)[0]
+    bioguide_1 = legislator_1.bioguide_id
+    bioguide_2 = legislator_2.bioguide_id
+    return redirect('membermash-legislators-mash', bioguide_1=bioguide_1,
+        bioguide_2=bioguide_2)
